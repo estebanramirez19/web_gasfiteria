e8f8520eae8929f9a1badb38d612c6135c8ab18c
estebanramirez19 <esteban.ramirez19@inacapmail.cl>
2025-12-11 13:53:06 -0300

    comienzo de la creacion del header

--- gasfiteria-frontend/src/components/Header.tsx
+++ gasfiteria-frontend/src/components/Header.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
 
+import { Navbar, Container, Nav, NavDropdown} from 'react-bootstrap';
+import "../css/Header.css";
 
 function NavbarHeader() {
     return (
         <>
-        <header>
-            <div>hola</div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container className='navHeader'>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
 
-        
-        </header>
+
+            <Nav className="me-auto">
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                    Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+
+                <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                </NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+          
+        </Container>
+        </Navbar>
+
         
         </>
         
